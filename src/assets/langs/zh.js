3454dac8d34b8795ee59a8de511fb2ff6d4b67c4
leiyoujia <47096111+leiyoujia@users.noreply.github.com>
2020-02-22 20:46:59 +0800

    增加 Tab，调整样式

--- src/assets/langs/zh.js
+++ src/assets/langs/zh.js
@@ -2,5 +2,14 @@ export default {
     title: "天天基金",
     labels: {
         price: "基金净值",
+        estimation: "净值估算",
+        ranking: "基金排行",
+        optional: "自选基金",
+        fundBar: "基金吧",
+        account: "免费开户",
+        trade: "基金交易",
+        buy: "基金导购",
+        current: "活期宝",
+        vip: "高端理财",
     },
 };
