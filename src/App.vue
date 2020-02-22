e8f3d8cced72254b7b0fbf3d603d841457dc7daa
leiyoujia <1105167749@qq.com>
2020-02-22 19:43:10 +0800

    加入 Tab 栏

--- src/App.vue
+++ src/App.vue
@@ -1,55 +1,133 @@
 <script>
-import HelloWorld from "./components/HelloWorld";
+import NavDrawers from "./components/NavDrawers.vue";
+import NavTab from "./components/Tab.vue";
 
 export default {
-    name: "App",
     components: {
-        HelloWorld
+        NavDrawers,
+        NavTab,
     },
-
     data: () => ({
-        //
-    })
+        drawer: null,
+        tabList: {
+            price: "chart-bar",
+            estimation: "chart-line",
+            ranking: "format-list-numbered",
+            optional: "clipboard-plus-outline",
+            fundBar: "comment-multiple-outline",
+            account: "account-check-outline",
+            trade: "currency-usd-circle-outline",
+            buy: "cart-plus",
+            current: "home-currency-usd",
+            vip: "credit-card-settings-outline",
+        },
+    }),
+    created() {
+        this.$vuetify.theme.light = true;
+    },
 };
 </script>
 
 <template>
-    <v-app>
-        <v-app-bar app color="primary" dark>
-            <div class="d-flex align-center">
-                <v-img
-                    alt="Vuetify Logo"
-                    class="shrink mr-2"
-                    contain
-                    src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
-                    transition="scale-transition"
-                    width="40"
-                />
+    <v-app id="found-fund">
+        <v-navigation-drawer
+            v-model="drawer"
+            app
+            clipped
+        >
+            <nav-drawers />
+        </v-navigation-drawer>
 
-                <v-img
-                    alt="Vuetify Name"
-                    class="shrink mt-1 hidden-sm-and-down"
-                    contain
-                    min-width="100"
-                    src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
-                    width="100"
-                />
-            </div>
-
-            <v-spacer></v-spacer>
-
-            <v-btn
-                href="https://github.com/vuetifyjs/vuetify/releases/latest"
-                target="_blank"
-                text
+        <v-app-bar
+            app
+            clipped-left
+            color="red"
+            dense
+        >
+            <v-app-bar-nav-icon @click.stop="drawer = !drawer" />
+            <v-icon class="mx-4">
+                fab fa-youtube
+            </v-icon>
+            <v-toolbar-title class="mr-12 align-center">
+                <span class="title font-face">
+                    {{ $t("title") }}
+                </span>
+            </v-toolbar-title>
+            <v-spacer />
+            <v-row
+                align="center"
+                style="max-width: 650px"
             >
-                <span class="mr-2">Latest Release</span>
-                <v-icon>mdi-open-in-new</v-icon>
-            </v-btn>
+                <v-text-field
+                    :append-icon-cb="() => {}"
+                    placeholder="Search..."
+                    single-line
+                    append-icon="search"
+                    color="white"
+                    hide-details
+                />
+            </v-row>
         </v-app-bar>
 
         <v-content>
-            <HelloWorld/>
+            <v-container class="fill-height">
+                <nav-tab
+                    icon="chart-bar"
+                    text="labels.price"
+                />
+            <!--                <v-row-->
+            <!--                    justify="center"-->
+            <!--                    align="center"-->
+            <!--                >-->
+            <!--                    <v-col class="shrink">-->
+            <!--                        <v-tooltip right>-->
+            <!--                            <template v-slot:activator="{ on }">-->
+            <!--                                <v-btn-->
+            <!--                                    :href="source"-->
+            <!--                                    icon-->
+            <!--                                    large-->
+            <!--                                    target="_blank"-->
+            <!--                                    v-on="on"-->
+            <!--                                >-->
+            <!--                                    <v-icon large>-->
+            <!--                                        mdi-code-tags-->
+            <!--                                    </v-icon>-->
+            <!--                                </v-btn>-->
+            <!--                            </template>-->
+            <!--                            <span>Source</span>-->
+            <!--                        </v-tooltip>-->
+            <!--                        <v-tooltip right>-->
+            <!--                            <template v-slot:activator="{ on }">-->
+            <!--                                <v-btn-->
+            <!--                                    icon-->
+            <!--                                    large-->
+            <!--                                    href="https://codepen.io/johnjleider/pen/aezMOO"-->
+            <!--                                    target="_blank"-->
+            <!--                                    v-on="on"-->
+            <!--                                >-->
+            <!--                                    <v-icon large>-->
+            <!--                                        mdi-codepen-->
+            <!--                                    </v-icon>-->
+            <!--                                </v-btn>-->
+            <!--                            </template>-->
+            <!--                            <span>Codepen</span>-->
+            <!--                        </v-tooltip>-->
+            <!--                    </v-col>-->
+            <!--                </v-row>-->
+            </v-container>
         </v-content>
     </v-app>
 </template>
+
+<style lang="scss">
+/*.theme--light.v-sheet {*/
+/*    color: azure;*/
+/*}*/
+
+#found-fund {
+    .font-face {
+        font-family: "Noto Sans SC" !important;
+    }
+}
+
+</style>
