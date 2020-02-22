ded7d451af4f5c2dd96b89f8334d602bb7660e46
leiyoujia <1105167749@qq.com>
2020-02-22 20:46:59 +0800

    增加 Tab，调整样式

--- src/components/Tab.vue
+++ src/components/Tab.vue
@@ -10,21 +10,21 @@ export default {
             return `mdi-${this.icon}`;
         },
         displayText() {
-            return this.$t(this.text);
+            return this.$t(`labels.${this.text}`);
         },
     },
 };
 </script>
 
 <template>
-    <v-card hover>
+    <v-card class="ma-2" hover>
         <v-icon
             color="red lighten-1"
-            size="80"
+            size="60"
         >
             {{ iconName }}
         </v-icon>
-        <v-card-text class="pa-2">
+        <v-card-text class="pa-0">
             <span class="font-face">
                 {{ displayText }}
             </span>
