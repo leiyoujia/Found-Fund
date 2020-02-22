d99a3e23b37c281c32cf075c6fe56abbabf8dded
leiyoujia <47096111+leiyoujia@users.noreply.github.com>
2020-02-22 19:43:10 +0800

    加入 Tab 栏

---
+++ src/components/Tab.vue
@@ -0,0 +1,37 @@
+<script>
+export default {
+    name: "NavTab",
+    props: {
+        icon: { type: String, require: true },
+        text: { type: String, require: true },
+    },
+    computed: {
+        iconName() {
+            return `mdi-${this.icon}`;
+        },
+        displayText() {
+            return this.$t(this.text);
+        },
+    },
+};
+</script>
+
+<template>
+    <v-card hover>
+        <v-icon
+            color="red lighten-1"
+            size="80"
+        >
+            {{ iconName }}
+        </v-icon>
+        <v-card-text class="pa-2">
+            <span class="font-face">
+                {{ displayText }}
+            </span>
+        </v-card-text>
+    </v-card>
+</template>
+
+<style scoped>
+
+</style>
